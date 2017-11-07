a431212a83ec345f5669169c1d497259e0426df4
keemor <keemor@keemor.com>
2017-11-07 13:15:55 +0100

    Disable HighLight due to big budle size

--- src/components/highlight.js
+++ src/components/highlight.js
@@ -1,8 +1,13 @@
 import { h, render, Component } from "preact";
-import { HighLight, THEME } from "preact-highlight";
+//import { HighLight, THEME } from "preact-highlight";
 
 export default class HighLightJS extends Component {
     render({ code }) {
-        return <HighLight language="JavaScript" className="cmp-high-light" code={code} theme={THEME.monokaiSublime} />;
+        return (
+            <pre>
+                <code>{code}</code>
+            </pre>
+        );
+        //return <HighLight language="JavaScript" className="cmp-high-light" code={code} theme={THEME.monokaiSublime} />;
     }
 }
