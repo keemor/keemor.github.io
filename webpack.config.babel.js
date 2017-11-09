caa0195f50d5b8dce17e736bf3308c3573f0bcf4
keemor <keemor@keemor.com>
2017-11-09 12:43:36 +0100

    Disable source map in prod build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -8,67 +8,69 @@ import { getIfUtils, removeEmpty } from "webpack-config-utils";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export default env => {
-    const { ifProd, ifNotProd, ifTest } = getIfUtils(env);
+  const { ifProd, ifNotProd, ifTest } = getIfUtils(env);
 
-    return {
-        devtool: ifNotProd("cheap-module-source-map"),
-        entry: ["./src/index.js"],
-        output: {
-            filename: "./dist/bundle.js"
+  return {
+    devtool: ifProd("", "cheap-module-source-map"),
+    entry: ["./src/index.js"],
+    output: {
+      filename: "./dist/bundle.js"
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          use: ["babel-loader"]
         },
-        module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    use: ["babel-loader"]
-                },
-                {
-                    test: /\.scss$|\.css$/,
-                    use: ["style-loader", "css-loader", "sass-loader"]
-                }
-            ]
-        },
-        resolve: {
-            extensions: [".js"],
-            alias: {
-                ["~"]: resolve(__dirname, "src")
-            }
-        },
-        plugins: removeEmpty([
-            new webpack.DefinePlugin({
-                "process.env.NODE_ENV": JSON.stringify(ifProd("production", "development"))
-            }),
-            ifProd(
-                new webpack.optimize.UglifyJsPlugin({
-                    ie8: false,
-                    ecma: 8,
-                    output: {
-                        comments: false,
-                        beautify: false
-                    },
-                    parallel: {
-                        cache: true,
-                        workers: 2
-                    }
-                })
-            ),
-            new HtmlWebpackPlugin({
-                title: "keemor.github.io",
-                template: "src/index.ejs",
-                filename: "index.html"
-            }),
-            new OfflinePlugin({
-                ServiceWorker: {
-                    events: true
-                }
-            }),
-            ifTest(new BundleAnalyzerPlugin())
-        ]),
-        devServer: {
-            host: "localhost",
-            port: 3333,
-            watchContentBase: true,
-            historyApiFallback: true
+        {
+          test: /\.scss$|\.css$/,
+          use: ["style-loader", "css-loader", "sass-loader"]
         }
-    };
+      ]
+    },
+    resolve: {
+      extensions: [".js"],
+      alias: {
+        ["~"]: resolve(__dirname, "src")
+      }
+    },
+    plugins: removeEmpty([
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(
+          ifProd("production", "development")
+        )
+      }),
+      ifProd(
+        new webpack.optimize.UglifyJsPlugin({
+          ie8: false,
+          ecma: 8,
+          output: {
+            comments: false,
+            beautify: false
+          },
+          parallel: {
+            cache: true,
+            workers: 2
+          }
+        })
+      ),
+      new HtmlWebpackPlugin({
+        title: "keemor.github.io",
+        template: "src/index.ejs",
+        filename: "index.html"
+      }),
+      new OfflinePlugin({
+        ServiceWorker: {
+          events: true
+        }
+      }),
+      ifTest(new BundleAnalyzerPlugin())
+    ]),
+    devServer: {
+      host: "localhost",
+      port: 3333,
+      watchContentBase: true,
+      historyApiFallback: true
+    }
+  };
 };
