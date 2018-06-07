c1bf60608ee8e22969f19afd70059121ce5be3ad
keemor <keemor@keemor.com>
2018-06-07 14:32:59 +0200

    AsyncRoute test

--- src/index.js
+++ src/index.js
@@ -1,72 +1,77 @@
-import { h, render, Component } from "preact";
-import { Router } from "preact-router";
-import { createHashHistory } from "history";
-import * as runtime from "offline-plugin/runtime";
+import { h, render, Component } from 'preact';
+import Router from 'preact-router';
+import AsyncRoute from 'preact-async-route';
+import { createHashHistory } from 'history';
+import * as runtime from 'offline-plugin/runtime';
 
-import "preact-material-components/Typography/style.css";
-import "preact-material-components/Theme/style.css";
-import "./style/style.scss";
-import "preact/debug";
+import 'preact-material-components/Typography/style.css';
+import 'preact-material-components/Theme/style.css';
+import './style/style.scss';
+import 'preact/debug';
 
-import Header from "./components/header";
+import Header from './components/header';
 
-import Home from "./route/home";
-import About from "./route/about";
+import Home from './route/home';
+import About from './route/about';
 
-import Hello from "./route/code/hello";
-import RouterCode from "./route/code/router";
-import OfflineCode from "./route/code/offline";
-import HighlightCode from "./route/code/highlight";
-import WhereAmI from "./route/code/whereami";
-import AsyncAwait from "./route/code/asyncawait";
+//import Hello from './route/code/hello';
+// import RouterCode from './route/code/router';
+// import OfflineCode from './route/code/offline';
+// import HighlightCode from './route/code/highlight';
+// import WhereAmI from './route/code/whereami';
+// import AsyncAwait from './route/code/asyncawait';
 
 class Index extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    runtime.install({
-      onUpdating: () => {
-        console.log("SW Event:", "onUpdating");
-      },
-      onUpdateReady: () => {
-        console.log("SW Event:", "onUpdateReady");
-        // Tells to new SW to take control immediately
-        runtime.applyUpdate();
-      },
-      onUpdated: () => {
-        console.log("SW Event:", "onUpdated");
-        // Reload the webpage to load into the new version
-        window.location.reload();
-      },
+        runtime.install({
+            onUpdating: () => {
+                console.log('SW Event:', 'onUpdating');
+            },
+            onUpdateReady: () => {
+                console.log('SW Event:', 'onUpdateReady');
+                // Tells to new SW to take control immediately
+                runtime.applyUpdate();
+            },
+            onUpdated: () => {
+                console.log('SW Event:', 'onUpdated');
+                // Reload the webpage to load into the new version
+                window.location.reload();
+            },
 
-      onUpdateFailed: () => {
-        console.log("SW Event:", "onUpdateFailed");
-      }
-    });
-  }
-  logPageView() {
-    window.dataLayer.push({ event: window.location.hash });
-  }
+            onUpdateFailed: () => {
+                console.log('SW Event:', 'onUpdateFailed');
+            }
+        });
+    }
+    logPageView() {
+        window.dataLayer.push({ event: window.location.hash });
+    }
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <div class="marginTop">
-          <Router history={createHashHistory()} onChange={this.logPageView}>
-            <Home default path="/" />
-            <About path="/about" />
-            <Hello path="/code/hello" />
-            <RouterCode path="/code/router" />
-            <OfflineCode path="/code/offline" />
-            <HighlightCode path="/code/highlight" />
-            <WhereAmI path="/code/whereami" />
-            <AsyncAwait path="/code/asyncawait" />
-          </Router>
-        </div>
-      </div>
-    );
-  }
+    getComponent(url, cb, props) {
+        return System.import('./route' + url).then(module => module.default);
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <div class="marginTop">
+                    <Router history={createHashHistory()} onChange={this.logPageView}>
+                        <Home default path="/" />
+                        <About path="/about" />
+                        <AsyncRoute path="/code/hello" getComponent={this.getComponent} />
+                        <AsyncRoute path="/code/router" getComponent={this.getComponent} />
+                        <AsyncRoute path="/code/offline" getComponent={this.getComponent} />
+                        <AsyncRoute path="/code/highlight" getComponent={this.getComponent} />
+                        <AsyncRoute path="/code/whereami" getComponent={this.getComponent} />
+                        <AsyncRoute path="/code/asyncawait" getComponent={this.getComponent} />
+                    </Router>
+                </div>
+            </div>
+        );
+    }
 }
 
 render(<Index />, document.body);
